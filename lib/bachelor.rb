328e4dab060a33f0c4fe0baef64014c182798d0e
Nicholas Dover <dovern42@gmail.com>
2019-02-07 22:17:24 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -8,6 +8,7 @@ def get_first_name_of_season_winner(data, season)
         binding.pry
         if key["status"] == "winner"
           winner = key["name"].split(" ")[0]
+        end
       end
     end
   end
